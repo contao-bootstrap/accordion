9153abf6971423d0e6dc329cc5a2f9e4b40ffe0e
David Molineus <david.molineus@netzmacht.de>
2023-06-12 15:26:10 +0200

    Prefix with bootstrap

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,28 +1,19 @@
 <?php
 
-/**
- * Contao Bootstrap Accordion.
- *
- * @package    contao-bootstrap
- * @subpackage Accordion
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2014-2020 netzmacht David Molineus. All rights reserved.
- * @license    LGPL-3.0-or-later https://github.com/contao-bootstrap/Accordion/blob/master/LICENSE
- * @filesource
- */
+declare(strict_types=1);
 
 /*
  * content elements
  */
 $GLOBALS['TL_LANG']['CTE']['bs_accordion'] = 'Accordion';
 
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_start'][0] = 'Accordion group start';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_start'][1] = 'Accordion group start element';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_end'][0]   = 'Accordion group end';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_end'][1]   = 'Accordion group end element';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_start'][0]       = 'Accordion start';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_start'][1]       = 'Accordion start element';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_end'][0]         = 'Accordion end';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_end'][1]         = 'Accordion end element';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_single'][0]      = 'Accordion single';
-$GLOBALS['TL_LANG']['CTE']['bs_accordion_single'][1]      = 'Accordion single element';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_start'][0] = 'Bootstrap accordion group start';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_start'][1] = 'Bootstrap accordion group start element';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_end'][0]   = 'Bootstrap accordion group end';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_end'][1]   = 'Bootstrap accordion group end element';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_start'][0]       = 'Bootstrap accordion start';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_start'][1]       = 'Bootstrap accordion start element';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_end'][0]         = 'Bootstrap accordion end';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_end'][1]         = 'Bootstrap accordion end element';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_single'][0]      = 'Bootstrap accordion single';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_single'][1]      = 'Bootstrap accordion single element';
