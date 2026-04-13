f4186e1152325d45076e576f45bd8911766ff1fd
c.boelter <christopher@boelter.eu>
2026-04-13 09:40:33 +0200

    add missing translations

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -17,3 +17,8 @@ $GLOBALS['TL_LANG']['CTE']['bs_accordion_end'][0]         = 'Bootstrap accordion
 $GLOBALS['TL_LANG']['CTE']['bs_accordion_end'][1]         = 'Bootstrap accordion end element';
 $GLOBALS['TL_LANG']['CTE']['bs_accordion_single'][0]      = 'Bootstrap accordion single';
 $GLOBALS['TL_LANG']['CTE']['bs_accordion_single'][1]      = 'Bootstrap accordion single element';
+
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_wrapper']['0'] = 'Bootstrap accordion group wrapper';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_group_wrapper']['1'] = 'Bootstrap accordion group wrapper element';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_wrapper']['0']       = 'Bootstrap accordion wrapper';
+$GLOBALS['TL_LANG']['CTE']['bs_accordion_wrapper']['1']       = 'Bootstrap accordion wrapper element';
