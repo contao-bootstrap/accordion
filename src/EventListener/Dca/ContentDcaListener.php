c11bf05d081e2a51a7b8e2e9f9a644096ffd5a8b
David Molineus <david.molineus@netzmacht.de>
2026-04-28 15:20:41 +0200

    Replace @Callback annotation with #[AsCallback] attribute

--- src/EventListener/Dca/ContentDcaListener.php
+++ src/EventListener/Dca/ContentDcaListener.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace ContaoBootstrap\Accordion\EventListener\Dca;
 
-use Contao\CoreBundle\ServiceAnnotation\Callback;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\DataContainer;
 
 final class ContentDcaListener
 {
-    /**
-     * Generate a accordion name if not given.
-     *
-     * @param string|null   $value         Accordion name.
-     * @param DataContainer $dataContainer Data container driver.
-     *
-     * @Callback(table="tl_content", target="fields.bs_accordion_name.save")
-     */
+    #[AsCallback(table: 'tl_content', target: 'fields.bs_accordion_name.save')]
     public function generateAccordionName(string|null $value, DataContainer $dataContainer): string
     {
         /** @psalm-suppress RiskyTruthyFalsyComparison */
